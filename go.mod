388bc1fafa63d56fca2e1cb4643bd15be48ad1db
fandu139 <fandupratama@gmail.com>
2019-12-04 11:17:40 +0700

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/fandu139/admBackEnd
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185 // indirect
+	github.com/gin-gonic/gin v1.5.0
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/kardianos/govendor v1.0.9 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	golang.org/x/tools v0.0.0-20191204011308-9611592c72f6 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"io"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"os"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello Word")
+}
+
 func main() {
-	route := gin.Default()
-	route.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "Server Sudah Jalan",
-		})
-	})
+	// route := gin.Default()
+	// route.GET("/", func(context *gin.Context) {
+	// 	context.JSON(http.StatusOK, gin.H{
+	// 		"message": "Server Sudah Jalan",
+	// 	})
+	// })
 
-	route.POST("/auth", func(context *gin.Context) {
-		// context.JSON(http.StatusOK, gin.H{
-		// 	"message": "Server Sudah Jalan",
-		// })
-		// id := context.Query("id")
-		// page := context.DefaultQuery("page", "0")
-		name := context.PostForm("name")
-		message := context.PostForm("message")
+	// route.POST("/auth", func(context *gin.Context) {
+	// 	// context.JSON(http.StatusOK, gin.H{
+	// 	// 	"message": "Server Sudah Jalan",
+	// 	// })
+	// 	// id := context.Query("id")
+	// 	// page := context.DefaultQuery("page", "0")
+	// 	name := context.PostForm("name")
+	// 	message := context.PostForm("message")
 
-		context.JSON(200, gin.H{
-			"status":  "posted",
-			"message": message,
-			"name":    name,
-		})
+	// 	context.JSON(200, gin.H{
+	// 		"status":  "posted",
+	// 		"message": message,
+	// 		"name":    name,
+	// 	})
 
-		// fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
-	})
-	route.Run(":8089") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// 	// fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	// })
+	// route.Run(":8089") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":"+port, nil)
 }
